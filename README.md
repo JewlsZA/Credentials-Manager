83d25ed622d8c49ded705fe9c0053bfb2119e158
Julian Mackay <69505959+JewlsZA@users.noreply.github.com>
2021-11-12 17:35:39 +0200

    Adding project files

---
+++ README.md
@@ -0,0 +1,23 @@
+# Setup Credential Manager
+
+To run the program on your system you would need to install React and Node.js
+Here is a quick guide to get you started: https://www.freecodecamp.org/news/install-react-with-create-react-app/
+
+Open the project files in your favorite IDE and Change Directory to the project in your terminal of choice.
+
+To install dependancies run the following in the terminal.
+```
+npm install
+```
+To start your local react and express servers, navigate to the backend and frontend file directory and run the following command in the terminal.
+```
+npm start 
+```
+a Browser windows should open where you can interact with the program !
+If the browser does not open type http://localhost:3000/ in your browser
+
+
+# Getting Started with Credential Manager
+
+Interfacing with the program for the first time it is recommended to create a new user. User privilages can only be assigned by a admin. 
+The default admin account you can use to perform such functions is usr: julian psw: 123
---
+++ backend/backend_express_server.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const fetch = require("isomorphic-fetch")
+const helmet = require("helmet")
+const fs = require("fs")
+const app = express()
+const port = process.env.PORT || 8080
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const Routes = require("../backend/routes/routes.js")
+const jwt = require("jsonwebtoken")
+app.use(helmet())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(express.static("public"))
+app.use("/", Routes)
+
+//Mongoose connection
+const uri =
+  "email me for access to project database"
+mongoose.Promise = global.Promise
+mongoose.connect(uri, {
+  dbName: "projects"
+})
+mongoose.connection.on("error", function () {
+  console.log("Could not connect to the database. Exiting now...")
+  process.exit()
+})
+mongoose.connection.once("open", function () {
+  console.log("Successfully connected to the database")
+})
+
+//The ​listen() method specifies what port the app object (application server) will listen to HTTP requests on.
+app.listen(port, () => console.log(`Listening on port ${port}`))
